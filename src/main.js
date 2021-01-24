3e54b05a17c16e973b69fa964b7feb62cbb8435d
Yann MICHAUX <yann.michaux@outlook.fr>
2021-01-24 18:29:11 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ const Discord = require(`discord.js`)
 const { Player } = require(`discord-music-player`)
 const config = require(`./config`)
 const logger = require(`./lib/logger`)
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const client = new Discord.Client()
 const player = new Player(client, {
